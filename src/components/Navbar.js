630479e9156b0cb71a6a2edcee36983c7e9be0ea
RicardoTS15 <ricardots211@gmail.com>
2019-04-01 20:50:56 -0600

    NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import './css/theme.css'
 class Navbar extends React.Component{
     render(){
-        return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        return <nav className="navbar navbar-expand-lg navbar-dark main-light-2 fixed-top">
         <a className="navbar-brand" href="/">
-        <span className="font-weight-bold">Sin</span>
-        <span className="font-weight-light">Azucar</span>
+        <span className="font-weight-bold text-white">Sin</span>
+        <span className="font-weight-light">Azúcar</span>
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Inicio<span className="sr-only">(current)</span></a>
+            <li className="nav-item">
+              <a className="nav-link active" href="/">Inicio</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">Recetas</a>
@@ -24,16 +24,7 @@ class Navbar extends React.Component{
             <li className="nav-item">
               <a className="nav-link" href="/">Consejos</a>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown link
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="/">Action</a>
-                <a className="dropdown-item" href="/">Another action</a>
-                <a className="dropdown-item" href="/">Something else here</a>
-              </div>
-            </li>
+            
           </ul>
         </div>
       </nav>
